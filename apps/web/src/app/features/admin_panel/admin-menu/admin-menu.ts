7b166fd83931001c8eb405dcdabd84c12bc6aaaf
afgssxhn <tujhl@inbox.ru>
2026-02-21 17:37:52 +0500

    feat: add angular admin routing with menu and users management pages

---
+++ apps/web/src/app/features/admin_panel/admin-menu/admin-menu.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-menu',
+  standalone: true,
+  templateUrl: './admin-menu.html',
+})
+export class AdminMenuComponent {}
