96b0b418c574218a84fe7576f39abeb2c8c1dab7
Ilya Karashchuk <karashchuk_iv@edu.surgu.ru>
2026-02-28 02:25:03 +0500

    feat(web): configure routes and setup payments

--- libs/web/applicant/src/lib/applicant.routes.ts
+++ libs/web/applicant/src/lib/applicant.routes.ts
@@ -1,4 +1,15 @@
 import { Route } from '@angular/router';
 import { Applicant } from './applicant/applicant';
 
-export const applicantRoutes: Route[] = [{ path: '', component: Applicant, children: [] }];
+export const applicantRoutes: Route[] = [
+  {
+    path: '',
+    component: Applicant,
+    children: [
+      {
+        path: 'checkout',
+        loadComponent: () => import('./features/checkout/checkout').then((m) => m.Checkout),
+      },
+    ],
+  },
+];
