6ac2bb5ede01da1590b741bcad26c0ec06cf1437
aleksandr <aleksandr.egorov@zyfra.com>
2026-04-11 11:45:18 +0300

    Слияние из main

--- libs/web/shared/ui/src/index.ts
+++ libs/web/shared/ui/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/transaction-table/transaction-table.models';
 export * from './lib/dashboard-layout/dashboard-layout';
 export * from './lib/placeholder-page/placeholder-page';
 export * from './lib/placeholder-page/placeholder-page-route';
+export * from './lib/features/auth-services/auth-services';
 // RPC / Auth infrastructure
 export * from './lib/rpc/token-store';
 export * from './lib/rpc/rpc-transport';
