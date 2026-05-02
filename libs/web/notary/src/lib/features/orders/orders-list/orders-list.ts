da4e1ee272acf1944487d20a8abef741a90d4190
Еременко Анастасия <eas-06@yandex.ru>
2026-05-02 22:06:51 +0500

    feat(notary): реализован личный кабинет нотариуса (dashboard, subscription, orders)

---
+++ libs/web/notary/src/lib/features/orders/orders-list/orders-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-orders-list',
+  imports: [],
+  templateUrl: './orders-list.html',
+  styleUrl: './orders-list.scss',
+})
+export class OrdersList {}
