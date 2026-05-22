7ed0fe8130ad18d7926694565710d1f57f5f375e
afgssxhn <tujhl@inbox.ru>
2026-05-23 03:22:44 +0500

    feat(bitrix-leads): add http client for crm.lead.add

--- libs/api/bitrix-leads/src/index.ts
+++ libs/api/bitrix-leads/src/index.ts
@@ -1,4 +1,6 @@
 export * from './lib/bitrix-leads/bitrix-leads.module';
 export * from './lib/bitrix-leads/bitrix-leads-config.service';
+export * from './lib/bitrix-leads/bitrix-leads-api.service';
+export * from './lib/bitrix-leads/bitrix-leads.tokens';
 export * from './lib/bitrix-leads/bitrix-leads.types';
 export * from './lib/bitrix-leads/bitrix-leads.errors';
--- libs/api/bitrix-leads/src/lib/bitrix-leads/bitrix-leads.module.ts
+++ libs/api/bitrix-leads/src/lib/bitrix-leads/bitrix-leads.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
+import axios from 'axios';
+
+import { BitrixLeadsApiService } from './bitrix-leads-api.service';
 import { BitrixLeadsConfigService } from './bitrix-leads-config.service';
+import { BITRIX_LEADS_HTTP_CLIENT } from './bitrix-leads.tokens';
 
 @Module({
-  providers: [BitrixLeadsConfigService],
-  exports: [BitrixLeadsConfigService],
+  providers: [
+    BitrixLeadsConfigService,
+    BitrixLeadsApiService,
+    {
+      provide: BITRIX_LEADS_HTTP_CLIENT,
+      useFactory: () =>
+        axios.create({
+          timeout: 10_000,
+          headers: { 'Content-Type': 'application/json' },
+        }),
+    },
+  ],
+  exports: [BitrixLeadsConfigService, BitrixLeadsApiService],
 })
 export class BitrixLeadsModule {}
