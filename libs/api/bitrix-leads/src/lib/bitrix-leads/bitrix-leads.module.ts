6aa674f257d466aef3cbdda1e2e3feda5ef86658
afgssxhn <tujhl@inbox.ru>
2026-05-23 10:17:39 +0500

    feat(bitrix-leads): register publisher in module

--- libs/api/bitrix-leads/src/lib/bitrix-leads/bitrix-leads.module.ts
+++ libs/api/bitrix-leads/src/lib/bitrix-leads/bitrix-leads.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@internal/prisma';
 import axios from 'axios';
 
+import { BitrixLeadPublisherService } from './bitrix-lead-publisher.service';
 import { BitrixLeadsApiService } from './bitrix-leads-api.service';
 import { BitrixLeadsConfigService } from './bitrix-leads-config.service';
 import { BITRIX_LEADS_HTTP_CLIENT } from './bitrix-leads.tokens';
 
 @Module({
+  imports: [PrismaModule],
   providers: [
     BitrixLeadsConfigService,
     BitrixLeadsApiService,
+    BitrixLeadPublisherService,
     {
       provide: BITRIX_LEADS_HTTP_CLIENT,
       useFactory: () =>
@@ -18,6 +22,10 @@ import { BITRIX_LEADS_HTTP_CLIENT } from './bitrix-leads.tokens';
         }),
     },
   ],
-  exports: [BitrixLeadsConfigService, BitrixLeadsApiService],
+  exports: [
+    BitrixLeadsConfigService,
+    BitrixLeadsApiService,
+    BitrixLeadPublisherService,
+  ],
 })
 export class BitrixLeadsModule {}
